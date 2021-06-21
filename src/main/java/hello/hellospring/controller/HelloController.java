e5204c60f491762480dcd06adaa0d78b5d7079c5
Seong Hoon Wee <mrgravity817@gmail.com>
2021-06-22 02:13:30 +0900

    MVC and APIs

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,6 +12,42 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "Tripboi!");
         return "hello"; // opens "returnValue".html
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // we will directly inject return value into <body> tag.
+    // So we cannot see the full html file!
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        // When Response body return type is Object, it renders to JSON format. (JsonConverter)
+        //                                   String, it renders to String format. (StringConverter)
+        // JsonConverter + StringConverter = HttpMessageConverter
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
 
